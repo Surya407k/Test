0ac676b2d78890b4db01c567b244ad02a2caa063
Surya407k <166426046+Surya407k@users.noreply.github.com>
2024-05-11 01:29:36 +0530

    Add files via upload

---
+++ Refersh the Page1.py
@@ -0,0 +1,76 @@
+1.
+What was the purpose of adding the CTA combo class to the final Nav link in the Calendar site build?
+Scored 1 of 1 point
+2.
+Where are HTML elements & CSS properties located in the Designer?
+Scored 1 of 1 point
+3.
+How does a combo class differ from a regular class?
+Scored 1 of 1 point
+4.
+Why is understanding the box model important to web design?
+Scored 1 of 1 point
+5.
+How did we group multiple elements into a single CSS grid cell to create each card? drag
+Scored 0 of 1 point
+6.
+What is the simplest way to add default alt text to any instance of an image that doesn’t already have alt text associated with it? assert panel
+Scored 1 of 1 point
+7.
+You cannot make changes to your site once you click the Publish button.
+Scored 1 of 1 point
+8.
+A CSS class stores styling information for any element that shares the class.
+Scored 1 of 1 point
+9.
+CSS selectors target HTML elements and apply styles to them.
+Scored 1 of 1 point
+10.
+What is the purpose of the Webflow Designer? Select all that apply.
+Scored 0 of 1 point
+11.
+What does creating a swatch in the Style panel allow you to do? 
+
+Scored 0 of 1 point
+12.
+The Form block is made up of which child element(s)? Select all that apply.
+Scored 1 of 1 point
+13.
+HTML defines styling and layout, while CSS structures the content and page layout.
+Scored 1 of 1 point
+14.
+Which of the following is a state you can style on elements? 
+Scored 1 of 1 point
+15.
+If you delete an image while on the mobile landscape breakpoint, which breakpoint will the image still be visible on?
+Scored 0 of 1 point
+16.
+Related to the box model, which of the following are true? Select all that apply.
+Scored 1 of 1 point
+17.
+On the Site Settings page, which tab allows you to add your custom domain?
+Scored 1 of 1 point
+18.
+Why is it important to add alt text to all images that have meaningful content? Select all that apply.
+Scored 0.33 of 1 point
+19.
+What is the primary element we used to organize content on the Calendar App site?
+Scored 1 of 1 point
+20.
+What does CSS stand for?
+Scored 1 of 1 point
+21.
+When we styled the Calendar App site button, what was automatically created? a class
+Scored 1 of 1 point
+22.
+You cannot add third-party fonts to your site in Webflow.
+Scored 1 of 1 point
+23.
+What is one way to set the base font that can be inherited by all text elements on a page?
+Scored 1 of 1 point
+24.
+Where do you upload & manage your website’s assets in the Designer?
+Scored 1 of 1 point
+25.
+In order to set the base styling on all H3 tags on your site you need to:
+Scored 1 of 1 point
